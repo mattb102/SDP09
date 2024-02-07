3840fa552618033db67df3a5d96856375358112f
Aaron Bolduc <abolduc415@gmail.com>
2024-02-07 16:03:34 -0500

    lowercase file names, update imports, update routes

--- Senior-Design-Project-Front-End/src/App.js
+++ Senior-Design-Project-Front-End/src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
-import Welcome from './components/Welcome';
-import Login from './pages/Login/Login';
-import Root from './components/Root';
-import SignUp from './components/SignUp';
+import Login from './pages/Login/login';
+import Root from './components/root';
 import Home from './pages/Home/home';
 
 const router = createBrowserRouter(createRoutesFromElements(
