8da152a3208fa871ab5bb672ad119198dd15afe8
Aaron Bolduc <abolduc415@gmail.com>
2024-02-07 16:59:47 -0500

    rename files

--- Senior-Design-Project-Front-End/src/App.js
+++ Senior-Design-Project-Front-End/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import Login from './pages/Login/login';
+import SignUp from "./components/signup"
 import Root from './components/root';
 import Home from './pages/Home/home';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <>
     <Route index element={ <Login/> } />
+    <Route path="/signup" element={<SignUp />} />
     <Route path="/" element={ <Root/> }>
       <Route path="home" element={ <Home/> } />
     </Route>
