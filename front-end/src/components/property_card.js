283ba6dd15bf1877db696bcb4ac4b1932e4d84ec
Aaron Bolduc <abolduc415@gmail.com>
2024-03-27 16:33:57 -0400

    change image and information on cards

--- front-end/src/components/property_card.js
+++ front-end/src/components/property_card.js
@@ -6,19 +6,18 @@ function PropertyCard() {
     <Card maxW='sm'>
       <CardBody>
         <Image
-          src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+          src='https://photos.zillowstatic.com/fp/11bfacdab1f7910be8e606a470d9b4b8-cc_ft_1536.webp'
           alt='Green double couch with wooden legs'
           borderRadius='lg'
         />
-        <Stack mt='6' spacing='3'>
-          <Heading size='md'>Living room Sofa</Heading>
-          <Text>
-            This sofa is perfect for modern tropical spaces, baroque inspired
-            spaces, earthy toned spaces and for people who love a chic design with a
-            sprinkle of vintage design.
-          </Text>
+        <Stack mt='6' spacing='3' textAlign='left'>
+          <Heading size='md'>1 Minnesota Rd, # 1, Storrs Mansfield, CT 06268</Heading>
+          <Text>Bedrooms: 2</Text>
+          <Text>Bathrooms: 1.5</Text>
+          <Text>Size: 1,130 sqft</Text>
+          <Text></Text>
           <Text color='blue.600' fontSize='2xl'>
-            $450
+            $400,000
           </Text>
         </Stack>
       </CardBody>
