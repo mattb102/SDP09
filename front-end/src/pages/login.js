bda2194d58ca04b87d8dba1a424fff36830afdac
Aaron Bolduc <abolduc415@gmail.com>
2024-03-28 21:11:27 -0400

    redesign login

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -8,6 +8,8 @@ import {
   Heading,
   VStack,
   Link,
+  Flex,
+  Image,
 } from '@chakra-ui/react';
 
 const LoginPage = () => {
@@ -21,49 +23,74 @@ const LoginPage = () => {
   };
 
   return (
-    <Box
-      p={8}
-      maxWidth="md"
-      borderWidth={1}
-      borderRadius={8}
-      boxShadow="lg"
-      margin="auto"
-      mt={8}
+    <Flex
+      direction="row"
+      align="center"
+      justify="center"
+      height="100vh"
     >
-      <Heading mb={4}>Login</Heading>
-      <form onSubmit={handleSubmit}>
-        <VStack spacing={4}>
-          <FormControl id="email" isRequired>
-            <FormLabel>Email address</FormLabel>
-            <Input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </FormControl>
-          <FormControl id="password" isRequired>
-            <FormLabel>Password</FormLabel>
-            <Input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </FormControl>
-          <Button type="submit" colorScheme="blue" width="100%">
-            Login
-          </Button>
-          <Link href="#" color="blue.400">
-            Forgot password?
-          </Link>
-        </VStack>
-      </form>
-      <Box textAlign="center" mt={4}>
-        Don't have an account?{' '}
-        <Link href="#" color="blue.400">
-          Sign up
-        </Link>
+      {/* Logo on the left */}
+      <Box m={8}>
+        <Image src="logo.jpg" alt="Logo" h='85%' w='85%'/>
       </Box>
-    </Box>
+
+      {/* Form on the right */}
+      <Box
+        p={8}
+        m={8}
+        w='350px'
+        borderWidth={1}
+        borderRadius={8}
+        boxShadow="lg"
+        bg="white"
+      >
+        <Heading mb={4}>Login</Heading>
+        <form onSubmit={handleSubmit}>
+          <VStack spacing={4}>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                variant="outline"
+                focusBorderColor="gray.600"
+                borderRadius={0}
+              />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <Input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                variant="outline"
+                focusBorderColor="gray.600"
+                borderRadius={0}
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              colorScheme="gray"
+              width="100%"
+              borderRadius={0}
+              _hover={{ bg: 'gray.700' }}
+            >
+              Login
+            </Button>
+            <Link href="#" color="gray.600">
+              Forgot password?
+            </Link>
+          </VStack>
+        </form>
+        <Box textAlign="center" mt={4}>
+          Don't have an account?{' '}
+          <Link href="#" color="gray.600">
+            Sign up
+          </Link>
+        </Box>
+      </Box>
+    </Flex>
   );
 };
 
