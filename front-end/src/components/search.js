7bfd5660aaa05bbd66956973c697328c0ad52879
Aaron Bolduc <abolduc415@gmail.com>
2024-04-10 15:58:20 -0400

    change placeholder

--- front-end/src/components/search.js
+++ front-end/src/components/search.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex, Input, Button, Menu, MenuButton, MenuList, Text, Select } from '@chakra-ui/react';
+import { Box, Flex, Input, Button, Menu, MenuButton, MenuList, Text, Select, Stack } from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 
 import handleFilter from '../utilities/filter';
@@ -15,23 +15,25 @@ function SearchBar({setQueryParams, setCurrentPage}) {
             Filters
           </MenuButton>
           <MenuList borderRadius={0} p={2}>
-            <Text>Price Range:</Text>
-            <Flex>
-              <Input name="min_price" type="number" placeholder="Min" onChange={(e) => handleFilter(e, setQueryParams, setCurrentPage)}/>
-              <Input name="max_price" type="number" placeholder="Max" onChange={(e) => handleFilter(e, setQueryParams, setCurrentPage)}/>
-            </Flex>
-            <Text>Number of Bedrooms:</Text>
-            <Input name="beds_total" type="number" min="0" onChange={(e) => handleFilter(e, setQueryParams, setCurrentPage)}/>
-            <Text>Number of Bathrooms:</Text>
-            <Input name="baths_total" type="number" min="0" onChange={(e) => handleFilter(e, setQueryParams, setCurrentPage)}/>
-            <Text>Property Type:</Text>
-            <Select>
-              <option value="house">House</option>
-              <option value="apartment">Apartment</option>
-              <option value="CO">Condo</option>
-            </Select>
-            <Text>Location:</Text>
-            <Input type="text" placeholder="Location" onChange={(e) => handleFilter(e, setQueryParams, setCurrentPage)}/>
+            <Stack spacing={2}>
+              <Text>Price Range:</Text>
+              <Flex spacing={1}>
+                <Input name="min_price" type="number" placeholder="Minimum" onChange={(e) => handleFilter(e, setQueryParams, setCurrentPage)}/>
+                <Input name="max_price" type="number" placeholder="Maximum" onChange={(e) => handleFilter(e, setQueryParams, setCurrentPage)}/>
+              </Flex>
+              <Text>Number of Bedrooms:</Text>
+              <Input name="beds_total" type="number" min="0" onChange={(e) => handleFilter(e, setQueryParams, setCurrentPage)}/>
+              <Text>Number of Bathrooms:</Text>
+              <Input name="baths_total" type="number" min="0" onChange={(e) => handleFilter(e, setQueryParams, setCurrentPage)}/>
+              <Text>Property Type:</Text>
+              <Select>
+                <option value="house">House</option>
+                <option value="apartment">Apartment</option>
+                <option value="CO">Condo</option>
+              </Select>
+              <Text>Square Footage:</Text>
+              <Input name="sq_ft_total" type="number" min="0" onChange={(e) => handleFilter(e, setQueryParams, setCurrentPage)}/>
+            </Stack>
           </MenuList>
         </Menu>
       </Flex>
