3ad2d7b9dcd63b4f924082de0d13b1488c4192ac
Aaron Bolduc <abolduc415@gmail.com>
2024-04-15 00:24:38 -0400

    remove unused view that was causing run error

--- sdp_app/real_estate_app/urls.py
+++ sdp_app/real_estate_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HouseDetailView, HouseListView, SearchView
+from .views import HouseDetailView, HouseListView
 
 urlpatterns = [
     path('house/', HouseListView.as_view(), name='house-list'),
