272979151a5c0ae5008b9b23fb4c7077910cf94f
mattb102 <mattbeauvais52@gmail.com>
2024-04-16 08:04:25 -0400

    cleanup

--- sdp_app/real_estate_app/urls.py
+++ sdp_app/real_estate_app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import HouseDetailView, HouseListView, SearchView
-
+from .views import HouseDetailView, HouseListView
 urlpatterns = [
     path('house/', HouseListView.as_view(), name='house-list'),
     path('house/<int:pk>/', HouseDetailView.as_view(), name='house-detail'),
