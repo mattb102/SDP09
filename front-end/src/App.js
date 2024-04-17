6966027c19ebb075a22ca4f20fce7bbd26b1b779
Aaron Bolduc <abolduc415@gmail.com>
2024-04-17 17:49:35 -0400

    create 404 page for invalid routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } 
 
 import LoginPage from './pages/login';
 import Dashboard from './pages/dashboard'
+import PageNotFound from './pages/page-not-found'
 import Root from './components/root';
 
 import './App.css';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <>
+    <Route path="*" element={<PageNotFound />} />
     <Route path="/" element={ <Root/> }>
       <Route index element={ <LoginPage/> } />
       <Route path="/dashboard" element={ <Dashboard/> } />
